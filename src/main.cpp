2dd8ee2fe63257dcbfda96ad76c7dd956cfe382f
iwnmw <isaacbwoodward@gmail.com>
2025-06-16 11:11:44 -0700

    Quick updates that got missed in initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,26 @@
 #include <Arduino.h>
+#include "actuatorController.h"
 
-/*==========================================================================
-// Author : Handson Technology
-// Project : Arduino Uno
-// Description : L298N Motor Driver
-// Source-Code : L298N_Motor.ino
-// Program: Control 2 DC motors using L298N H Bridge Driver
-//==========================================================================
-*/
-// Definitions Arduino pins connected to input H Bridge
-int IN1 = 0;
-int IN2 = 1;
+// Define the Pin Numbers
+int ILI = 0; // Internal Logic Input
+int ELI = 1; // External Logic Input
+int potPin = 2; // Potentiometer Feedback Pin
+
+// Create an Instance of the ActuatorController Class
+ActuatorController actuatorController(potPin, ILI, ELI);
 
 void setup()
 {
 // Set the output pins
-pinMode(IN1, OUTPUT);
-pinMode(IN2, OUTPUT);
+pinMode(ILI, OUTPUT);
+pinMode(ELI, OUTPUT);
+pinMode(potPin, INPUT);
+
+Serial.begin(9600); // Start Serial Communication for Debugging
+actuatorController.setTargetPosition(50.0f); // Set the Target Position to 50mm
 }
 
 void loop()
 {
-// Rotate the Motor A clockwise
-digitalWrite(IN1, HIGH);
-digitalWrite(IN2, LOW);
-delay(2000);
-// Motor A
-digitalWrite(IN1, LOW);
-digitalWrite(IN2, HIGH);
-delay(2000);
-// Rotates the Motor A counter-clockwise
-// digitalWrite(IN1, LOW);
-// digitalWrite(IN2, HIGH);
-// delay(2000);
-// Motor A
-// digitalWrite(IN1, HIGH);
-// digitalWrite(IN2, HIGH);
-// delay(500);
+
 }
